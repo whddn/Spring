0df5f32d5234908366d290144e226d3081908a2d
종우 <ghjkl133120@naver.com>
2025-02-14 18:00:18 +0900

    Controller

--- boot01/src/main/java/com/yedam/app/emp/mapper/AaaMapper.java
+++
@@ -1,8 +0,0 @@
-package com.yedam.app.emp.mapper;
-
-import org.apache.ibatis.annotations.Insert;
-
-public interface AaaMapper {
-	@Insert("insert into tbl_aaa values(#{value}")
-	public int insert(String value);
-}
