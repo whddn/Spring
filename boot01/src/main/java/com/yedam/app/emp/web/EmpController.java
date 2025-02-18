49da266d70868510fb411a896df7a95c6337bd95
종우 <ghjkl133120@naver.com>
2025-02-18 17:49:22 +0900

    docs : Thymeleaf문법 || 태그

--- boot01/src/main/java/com/yedam/app/emp/web/EmpController.java
+++ boot01/src/main/java/com/yedam/app/emp/web/EmpController.java
@@ -37,11 +37,14 @@ public class EmpController {
 		model.addAttribute("emps", list);
 		// 3) View
 		return "emp/list";
+		
+		// prifix + return + suffix
+		// classpath:/templates/emp/list.html
 	}
 
 	// 단건조회 : GET + 전달받을 데이터 => QueryString
 	// 1) URL + METHOD
-	@GetMapping("empInfo")
+	@GetMapping("empInfo") //empInfo?employeeId=100
 	public String empInfo(EmpVO empVO, Model model) {
 		// 2) Service
 		EmpVO findVO = empService.findEmpInfo(empVO);
@@ -49,6 +52,9 @@ public class EmpController {
 		model.addAttribute("emp", findVO);
 		// 3) View
 		return "emp/info";
+		
+		// prifix + return + suffix
+		// classpath:/templates/emp/info.html
 	}
 
 	// 등록 - 페이지 : GET
